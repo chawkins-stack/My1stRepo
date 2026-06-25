7d1cbba37eb71679a2df983cf61230dee7035f5f
chawkins-stack <chawkins@catalyte.io>
2026-06-25 13:23:07 -0500

    Add files via upload

---
+++ Operators and Expressions Excercise/main.py
@@ -0,0 +1,26 @@
+# Bill 1
+
+
+bill = 275 
+
+tip = bill * 0.15 if bill >= 50 and bill <= 300 else bill * 0.20
+
+print(f"The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}")
+
+
+# # Bill 2
+
+bill = 40
+
+tip = bill * 0.15 if bill >= 50 and bill <= 300 else bill * 0.20
+
+print(f"The bill was {bill}, the tip was {tip}, and the total value was {bill + tip}")
+
+
+# Bill 3
+
+bill = 430
+
+tip = bill * 0.15 if bill >= 50 and bill <= 300 else bill * 0.20
+
+print(f"The bill was {bill}, the tip was {tip}, and the total value was {bill + tip}") 
---
+++ Variable and Data Exercise/variables-and-data-types-exercise-chawkins-stack/main.py
@@ -0,0 +1,70 @@
+# Level 1: The Virtual Warehouse
+
+data_center_name = "Omega-7"
+rack_count = 42
+temperature_celsius = 18.5
+is_operational = True
+
+print(data_center_name)
+print(rack_count)
+print(temperature_celsius)
+print(is_operational)
+
+
+# Level 2: The Data Audit
+
+value_a = 800
+value_b = "800"
+value_c = 0.0
+value_d = None
+value_e = False
+
+print(type(value_a))
+print(type(value_b))
+print(type(value_c))
+print(type(value_d))
+print(type(value_e))
+
+
+# Level 3: The System Refresh
+
+current_load = 100
+print(current_load)
+
+current_load = 55.5
+print(current_load)
+
+current_load = "Overload"
+print(current_load)
+
+
+# Level 4: The Logic Gate
+
+test_a = 0
+test_b = "0"
+test_c = ""
+test_d = " "
+test_e = None
+test_f = "False"
+
+print(bool(test_a))
+print(bool(test_b))
+print(bool(test_c))
+print(bool(test_d))
+print(bool(test_e))
+print(bool(test_f))
+
+
+# Level 5: The "Broken" Sensor
+
+sensor_input = ""
+
+is_sensor_active = bool(sensor_input)
+
+print("Sensor Active Status:", is_sensor_active)
+
+sensor_input = 22
+
+is_sensor_active = bool(sensor_input)
+
+print("Sensor Active Status:", is_sensor_active)
